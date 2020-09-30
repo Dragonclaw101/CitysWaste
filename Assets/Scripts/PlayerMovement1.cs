b81840d8a543f9a55432771fc61a44cfcf57411a
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2020-09-29 21:19:17 -0700

    Added first setup for game. Setup/testing only. 09/29/20

---
+++ Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    //Public vars
+    public float speed;
+    public float sprint;
+
+    //Private vars
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     if (Input.GetKey(KeyCode.W))
+        {
+            new Vector3(0, 0, speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            new Vector3(0, 0, -speed);
+        }
+
+    }
+}
