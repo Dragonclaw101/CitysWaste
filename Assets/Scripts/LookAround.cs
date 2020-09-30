0605ccb9a09301755065e0e9b8c2bd0648732ef6
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2020-09-30 10:04:08 -0700

    Added better scripting/rigidboy. 09/30/20

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+
+    [SerializeField] Transform cam;
+    [SerializeField] float sensitivity;
+    [SerializeField] float rotationLimit = 90f;
+
+
+    float headRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
+        transform.Rotate(0f, x, 0f);
+        headRotation += y;
+        headRotation = Mathf.Clamp(headRotation, -rotationLimit, rotationLimit);
+        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+    }
+}
