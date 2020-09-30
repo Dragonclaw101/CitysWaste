777dffaf0822a568fed4199011b137493b0d4632
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2020-09-29 23:32:29 -0700

    Added some basic movement scripting. 09/30/20

--- Assets/Scripts/PlayerMovement1.cs
+++ Assets/Scripts/PlayerMovement1.cs
@@ -4,31 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement1 : MonoBehaviour
 {
+
+    CharacterController characterController;
     //Public vars
     public float speed;
     public float sprint;
+    public float mouseSensitivity;
+
 
     //Private vars
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
+        //WASD movement
      if (Input.GetKey(KeyCode.W))
         {
-            new Vector3(0, 0, speed);
+            transform.position = transform.position + new Vector3(0, 0, speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            new Vector3(0, 0, -speed);
+            transform.position = transform.position + new Vector3(0, 0, -speed);
         }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-speed/2, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(speed/2, 0, 0);
+        }
     }
 }
